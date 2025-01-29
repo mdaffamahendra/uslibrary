baa418ba32b6c2af0e5a5b6443953d7bd3f92304
mdaffamahendra <daffa123mahendra@gmail.com>
2025-01-29 09:01:41 +0700

    fix flex search page

--- src/components/Fragment/LibrarySearch.jsx
+++ src/components/Fragment/LibrarySearch.jsx
@@ -75,7 +75,7 @@ const LibrarySearch = () => {
 
       {loading && <span className="loading loading-bars loading-lg text-blue-500 mx-auto mb-12"></span>} {/* Tampilkan loading jika sedang loading */}
       {error && <p style={{ color: "red" }}>{error}</p>} {/* Tampilkan error jika ada */}
-      <div className="flex flex-wrap gap-2">
+      <div className="flex flex-wrap gap-2 justify-center items-center">
         {books.length === 0 ? (
           <div className="w-full text-center p-4 text-gray-600">
             <p>Loading...</p> {/* Pesan jika tidak ada hasil */}
